ef56c841ce80dc8c408173929b07d46fc107eeb5
Ramveer <ramveerk7802@gmail.com>
2025-04-11 00:19:03 +0530

    Add enter

--- src/main/java/com/rvcode/E_Service/App/config/SecurityConfig.java
+++ src/main/java/com/rvcode/E_Service/App/config/SecurityConfig.java
@@ -50,6 +50,8 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
+    
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
